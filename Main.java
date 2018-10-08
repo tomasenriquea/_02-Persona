d80321d2ec811325ae939a9553ac736a6358948c
Tomas Enrique <tomasenriquea80@gmail.com>
2018-10-08 22:21:42 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+package uF4.trabajos.A2_03_10_2018._02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
+
+
+/*
+A2.2 Persona (public / private / protected / sin nada).
+Diseñar una clase Persona que tenga un identificador de usuario en un sistema, un nombre y apellidos reales, una edad y una clave 
+de acceso. Debe diseñar y programar la clase Java y sus operaciones de forma que:
+
+1 [] La edad se puede consultar y modificar en cualquier momento.
+2 [] Se puede consultar el identificador de usuario en cualquier momento, pero no se puede cambiar en ningún caso.
+3 [] No nos interesa conocer el nombre y apellidos reales.
+4 [] Sólo se puede cambiar la clave para una nueva. No se puede saber la clave antigua.
+
+*/
---
+++ Persona.java
@@ -0,0 +1,5 @@
+package uF4.trabajos.A2_03_10_2018._02;
+
+public class Persona {
+
+}
